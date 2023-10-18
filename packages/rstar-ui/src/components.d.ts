ac17d3c9742d946d0192635aa32f9c364a03ead4
jackma9604 <1074489519@qq.com>
2023-10-18 18:08:00 +0800

    chore: chore build

--- packages/rstar-ui/src/components.d.ts
+++ packages/rstar-ui/src/components.d.ts
@@ -1,6 +1,4 @@
-declare module '@vue/runtime-core' {
-  export interface GlobalComponents {
-    RSButton: typeof import('@rstar-ui/components/Button');
-  }
-}
-export {};
+declare const _default: {
+    RSButton: any;
+};
+export default _default;
--- scripts/build-components.js
+++ scripts/build-components.js
@@ -1,90 +1,84 @@
 import path from 'node:path'
-import fs from 'node:fs'
 import { fileURLToPath } from 'node:url'
 
-import { defineConfig, build } from 'vite'
+import { defineConfig, build, mergeConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJSX from '@vitejs/plugin-vue-jsx'
+import dts from 'vite-plugin-dts'
 // import version from '../package.json'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const baseConfig = defineConfig({
-  publicDir: false,
-  plugins: [vue(), vueJSX()],
-})
-const rollupOptions = defineConfig({
-  external: ['vue'],
-  globals: {
-    vue: 'Vue',
-  },
-})
 
 // 主入口
 const mainDir = path.resolve(__dirname, '../packages/rstar-ui')
 // 组件入口
 const componentsDir = path.resolve(__dirname, '../packages/components')
 // 输出目录
-const outputDir = path.resolve(__dirname, '../build')
+const outputDir = path.resolve(mainDir, 'dist')
 
+const baseConfig = defineConfig({
+  publicDir: false,
+  plugins: [vue(), vueJSX()],
+})
+const rollupOptions = defineConfig({
+  external: ['vue', 'unocss'],
+  globals: {
+    vue: 'Vue',
+  },
+})
+const alias = {}
 // 生成 package.json
-const createPackageJson = (name) => {
-  const fileStr = `{
-      "name": "${name ? name : 'rstar-ui'}",
-    //   "version": "${version}",
-      "description": "Vue3组件库",
-      "main": "${name ? 'index.umd.js' : 'rstar-ui.umd.js'}",
-      "module":"${name ? 'index.mjs' : 'rstar-ui.mjs'}",
-      "keywords": ["vue3", "组件库", "UI"],
-      "author": "jacma9604",
-      "license": "MIT" `
+// const createPackageJson = (name) => {
+//   const fileStr = `{
+//       "name": "${name ? name : 'rstar-ui'}",
+//     //   "version": "${version}",
+//       "description": "Vue3组件库",
+//       "main": "${name ? 'index.umd.js' : 'rstar-ui.umd.js'}",
+//       "module":"${name ? 'index.mjs' : 'rstar-ui.mjs'}",
+//       "keywords": ["vue3", "组件库", "UI"],
+//       "author": "jacma9604",
+//       "license": "MIT" `
 
-  const filePath = path.resolve(
-    outputDir,
-    name ? `${name}/package.json` : 'package.json'
-  )
-}
+//   const filePath = path.resolve(
+//     outputDir,
+//     name ? `${name}/package.json` : 'package.json'
+//   )
+// }
 /** 单组件按需构建 */
-
-const buildSingle = async (name) => {
+const buildSingle = async () => {
   await build(
-    defineConfig({
-      ...baseConfig,
-      build: {
-        lib: {
-          entry: path.resolve(componentsDir, name),
-          name: 'index',
-          fileName: 'index',
-          formats: ['es', 'umd'],
-        },
-        rollupOptions,
-        outDir: path.resolve(outputDir, name),
+    mergeConfig(mergeConfig(rollupOptions, baseConfig), {
+      publicDir: false,
+      resolve: {
+        alias: Object.entries(alias).map(([key, url]) => ({
+          find: key,
+          replacement: path.resolve(__dirname, url),
+        })),
       },
-    })
-  )
-}
+      plugins: [
+        dts({
+          tsconfigPath: path.resolve(__dirname, `../tsconfig.web.json`),
+          staticImport: true,
+          rollupTypes: true,
+          // outputDir: [`${outputDir}/types`],
+        }),
+      ],
 
-const buildAll = async () => {
-  await build(
-    defineConfig({
-      ...baseConfig,
       build: {
         lib: {
           entry: `${mainDir}/src/index.ts`,
           name: 'rstar-ui',
-          fileName: 'rstar-ui',
-          formats: ['es', 'umd'],
+          fileName: (format) => `index.js`,
+          formats: ['es'],
         },
-        rollupOptions,
         outDir: outputDir,
       },
     })
   )
-
-//   createPackageJson()
 }
 
 const buildLib = async () => {
-  await buildAll()
+  await buildSingle()
 
   //   按需打包
   //   fs.readdirSync(componentsDir)
